43e41b694601bc6e8e3709f55629703b9bdbe2c4
Scott Nielsen <snielsen@verisys.com>
2011-05-19 19:03:43 -0600

    Fixing the nesting of views to work correctly

--- droid_views.rb
+++ droid_views.rb
@@ -8,71 +8,68 @@ class DroidView
     'text_view'       => 'TextView',
     'relative_layout' => 'RelativeLayout',
     'image_view'      => 'ImageView',
-
+    'button'          => 'Button'
   }
 
   ATTRIBUTES = {
-    'orientation'   => 'orientation',
-    'layout_width'  => 'layout_width',
-    'layout_height' => 'layout_height',
-    'text'          => 'text',
-    'padding'       => 'padding',
-    'id'            => 'id',
-    'layout_align_parent_top'    => 'layout_alignParentTop',
-    'layout_align_parent_bottom' => 'layout_alignParentBottom',
-    'layout_align_parent_left'   => 'layout_alignParentLeft',
-    'layout_align_parent_right'  => 'layout_alignParentLeft',
-    'layout_margin_right' => 'layout_marginRight',
-    'layout_to_right_of'  => 'layout_toRightOf',
+    'orientation'                         => 'orientation',
+    'layout_width'                        => 'layout_width',
+    'layout_height'                       => 'layout_height',
+    'text'                                => 'text',
+    'padding'                             => 'padding',
+    'id'                                  => 'id',
+    'layout_align_parent_top'             => 'layout_alignParentTop',
+    'layout_align_parent_bottom'          => 'layout_alignParentBottom',
+    'layout_align_parent_left'            => 'layout_alignParentLeft',
+    'layout_align_parent_right'           => 'layout_alignParentLeft',
+    'layout_margin_right'                 => 'layout_marginRight',
+    'layout_to_right_of'                  => 'layout_toRightOf',
     'layout_align_with_parent_if_missing' => 'layout_alignWithParentIfMissing',
-    'layout_above' => 'layout_above',
-    'single_line'  => 'singleLine',
-    'ellipsize'    => 'ellipsize',
-    'text_color'   => 'textColor',
-    'gravity'      => 'gravity',
+    'layout_above'                        => 'layout_above',
+    'single_line'                         => 'singleLine',
+    'ellipsize'                           => 'ellipsize',
+    'text_color'                          => 'textColor',
+    'gravity'                             => 'gravity',
   }
-  
+
   CLASS_TYPES.each { |shortcut, actual|
     class_eval "def self.#{shortcut}(&block); create \"#{actual}\", block; end"
   }
 
-  ATTRIBUTES.each { |key, value| 
+  ATTRIBUTES.each { |key, value|
     define_method("#{key}=") { |arg| attribute(value, arg.to_s) }
   }
-  
+
   def self.create(name, block)
-    @@last_class ||= nil
-    @@first_class ||= nil
-    dv = DroidView.new(name, @@last_class)
-    @@first_class = dv unless @@first_class
-    @@last_class = dv
-    dv.fill_in(block)
+    @@current_parent ||= nil
+    dv = DroidView.new(name, @@current_parent)
+    @@root ||= dv
+    previous_parent, @@current_parent = @@current_parent, dv
+    block.call dv
+    @@current_parent = previous_parent
   end
 
   def self.render
     doc = Nokogiri::XML::Document.new
     doc.encoding = "UTF-8"
-    puts @@first_class.describe(doc)
-    @@first_class, @@last_class = nil
+    puts @@root.describe(doc)
+    @@root = nil
   end
 
-  def initialize(_name, _parent)
+  def initialize(_name, _parent=nil)
     @name = _name
-    family(_parent)
     @children = []
     @attributes = {}
+    unless _parent.nil?
+      @parent = _parent
+      @parent.child(self)
+    end
   end
 
-  def fill_in(block)
-    block.call self
+  def to_s
+    "<#{@name}>"
   end
 
-  def family(parent)
-    return unless parent
-    @parent = parent
-    parent.child(self)
-  end
- 
   def child(child)
     @children << child
   end
